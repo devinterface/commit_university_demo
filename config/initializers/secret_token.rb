b5f92766d8876b41b46fe45b11ca399c54829087
Stefano Mancini <stefano.mancini@devinterface.com>
2014-06-13 16:37:27 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CommitUniversityDemo::Application.config.secret_key_base = '5b94641c2432e003021d4aa710645ebc7c03eea71ecec21417876f62accc153887af5653d29eb4bc7ebde1db48bf883cbdc532d1c35dc9cdbd871e7f009d9a7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CommitUniversityDemo::Application.config.session_store :cookie_store, key: '_commit_university_demo_session'
